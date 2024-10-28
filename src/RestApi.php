4df0097b1381b54809ad1af08902a26ef0ee20ae
jauhar-imtikhan <bangjoe.muns@gmail.com>
2024-10-28 08:23:21 +0700

    add Class RestApi to comunicate from http

---
+++ src/RestApi.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Jnologi\RouterApi;
+
+use GuzzleHttp\Client;
+
+
+class RestApi
+{
+
+    protected int $port;
+    protected bool $isHttps = false;
+
+    public function __construct(
+        protected $ip,
+        protected string $username,
+        protected ?string $password,
+    ) {
+        //
+    }
+
+    public static function init($ip, $username, $password)
+    {
+        return new static($ip, $username, $password);
+    }
+
+    public function setPort($port): self
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    public function setToHttps($status = true): self
+    {
+        $this->isHttps = $status;
+        return $this;
+    }
+
+    public function get($query)
+    {
+
+        $url = sprintf('%s://%s:%s/rest/%s', $this->isHttps ? 'https' : 'http', $this->ip, $this->port, $query);
+
+        $client = new Client([
+            'base_uri' => $url,
+            'timeout'  => 30,
+            'auth'     => [$this->username, $this->password]
+        ]);
+
+        try {
+            $response = $client->request('GET', '', [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+            $results = $response->getBody()->getContents();
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $e->getMessage();
+        }
+        return $results;
+    }
+
+    public function add($query, $data)
+    {
+        $url = sprintf('%s://%s:%s/rest/%s', $this->isHttps ? 'https' : 'http', $this->ip, $this->port, $query);
+
+        $client = new Client([
+            'base_uri' => $url,
+            'timeout'  => 30,
+            'auth'     => [$this->username, $this->password]
+        ]);
+
+        try {
+
+            $response = $client->request('PUT', '', [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ],
+                'body' => json_encode($data)
+            ]);
+            $results = $response->getBody()->getContents();
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $e->getMessage();
+        }
+        return $results;
+    }
+
+    public function update($query, $data)
+    {
+        $url = sprintf('%s://%s:%s/rest/%s', $this->isHttps ? 'https' : 'http', $this->ip, $this->port, $query);
+
+        $client = new Client([
+            'base_uri' => $url,
+            'timeout'  => 30,
+            'auth'     => [$this->username, $this->password]
+        ]);
+
+        try {
+
+            $response = $client->request('PATCH', '', [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ],
+                'body' => json_encode($data)
+            ]);
+            $results = $response->getBody()->getContents();
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $e->getMessage();
+        }
+        return $results;
+    }
+
+    public function delete($query)
+    {
+        $url = sprintf('%s://%s:%s/rest/%s', $this->isHttps ? 'https' : 'http', $this->ip, $this->port, $query);
+
+        $client = new Client([
+            'base_uri' => $url,
+            'timeout'  => 30,
+            'auth'     => [$this->username, $this->password]
+        ]);
+
+        try {
+
+            $response = $client->request('DELETE', '', [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+            $results = $response->getBody()->getContents();
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return $e->getMessage();
+        }
+        return $results;
+    }
+}
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitb85563b167f027af597ff0735300fc38
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitb85563b167f027af597ff0735300fc38', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitb85563b167f027af597ff0735300fc38', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitb85563b167f027af597ff0735300fc38
         call_user_func(\Composer\Autoload\ComposerStaticInitb85563b167f027af597ff0735300fc38::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitb85563b167f027af597ff0735300fc38::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
